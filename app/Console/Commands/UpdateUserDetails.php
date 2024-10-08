d8e55254e6895db0a713af2559065a2582b79e18
Goudmane Oualid <walidlioon.09@gmail.com>
2024-10-08 17:15:11 +0100

    Added timezone field, seeded 20 users, and created artisan command to update user details

---
+++ app/Console/Commands/UpdateUserDetails.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\User;
+use Faker\Factory as Faker;
+
+class UpdateUserDetails extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:update-user-details';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Update users with random names and timezones';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $faker = Faker::create();
+        $timezones = ['CET', 'CST', 'GMT+1'];
+
+        $users = User::all();
+
+        // Loop through each user and update their details
+        foreach ($users as $user) {
+            $user->name = $faker->firstName . ' ' . $faker->lastName;
+            $user->timezone = $faker->randomElement($timezones);
+            $user->save();
+    
+            $this->info('Updated User ID: ' . $user->id . ' - ' . $user->name . ' - ' . $user->timezone);
+        }
+    }
+}
